ffa2e97b84326d822bd7727df51cad464bd4f2a5
mafuba <tulkidg@gmail.com>
2023-09-01 22:10:03 +0300

    feat: add id, name and year of birth fields

--- src/main/java/ru/nasrulaev/spring/models/Person.java
+++ src/main/java/ru/nasrulaev/spring/models/Person.java
@@ -1,4 +1,54 @@
 package ru.nasrulaev.spring.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "person")
 public class Person {
+
+    @Column(name = "user_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "full_name")
+    @NotBlank
+    @Size(min = 2, message = "Full name should contain at least 2 characters")
+    private String name;
+
+    @Column(name = "birth_year")
+    @Min(value = 1900, message = "Year of birth must be at least 1900")
+    private int birthYear;
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+
 }
