6bc12f479dab76ed0c11d7e0351cfbcc976bdc9a
mafuba <tulkidg@gmail.com>
2023-09-01 20:23:07 +0300

    feat: create transactionManager bean

--- src/main/java/ru/nasrulaev/spring/config/SpringConfig.java
+++ src/main/java/ru/nasrulaev/spring/config/SpringConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -91,5 +93,12 @@ public class SpringConfig implements WebMvcConfigurer {
         entityManagerFactory.setJpaProperties(hibernateProperties());
 
         return entityManagerFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
     }
 }
