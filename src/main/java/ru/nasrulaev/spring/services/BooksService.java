7f859fd15bd2f854837767a9132d441f019a33ed
mafuba <tulkidg@gmail.com>
2023-09-02 15:35:58 +0300

    feat(service): add booksRepository field

--- src/main/java/ru/nasrulaev/spring/services/BooksService.java
+++ src/main/java/ru/nasrulaev/spring/services/BooksService.java
@@ -1,9 +1,18 @@
 package ru.nasrulaev.spring.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.nasrulaev.spring.repositories.BooksRepository;
 
 @Service
 @Transactional
 public class BooksService {
+
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
 }
