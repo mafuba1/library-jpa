7d611e27d9a59c222241380444093c56c657c952
mafuba <tulkidg@gmail.com>
2023-09-02 13:38:15 +0300

    feat: add findBooksByTitleStartingWith method

--- src/main/java/ru/nasrulaev/spring/repositories/BooksRepository.java
+++ src/main/java/ru/nasrulaev/spring/repositories/BooksRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.nasrulaev.spring.models.Book;
 
+import java.util.List;
+
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findBooksByTitleStartingWith(String title);
 }
