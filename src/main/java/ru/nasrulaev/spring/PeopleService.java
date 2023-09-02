30594f2def4eb8c1a0a3262325303b7a1a9be705
mafuba <tulkidg@gmail.com>
2023-09-02 14:01:31 +0300

    feat(people): add findAll method

--- src/main/java/ru/nasrulaev/spring/PeopleService.java
+++ src/main/java/ru/nasrulaev/spring/PeopleService.java
@@ -1,9 +1,29 @@
 package ru.nasrulaev.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.nasrulaev.spring.models.Person;
+import ru.nasrulaev.spring.repositories.BooksRepository;
+import ru.nasrulaev.spring.repositories.PeopleRepository;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 public class PeopleService {
+
+    private PeopleRepository peopleRepository;
+    private BooksRepository booksRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository, BooksRepository booksRepository) {
+        this.peopleRepository = peopleRepository;
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
 }
