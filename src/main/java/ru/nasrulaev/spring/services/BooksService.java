d25bdcd88a55a8364b5485be08f5cc2a35ecdde5
mafuba <tulkidg@gmail.com>
2023-09-02 15:29:50 +0300

    feat: create BooksService class

---
+++ src/main/java/ru/nasrulaev/spring/services/BooksService.java
@@ -0,0 +1,9 @@
+package ru.nasrulaev.spring.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BooksService {
+}
