43124b9b1f9c5696ca4cb519256a547b6e4d8ab2
mafuba <tulkidg@gmail.com>
2023-09-09 18:01:40 +0300

    feat(controller): add show method

--- src/main/java/ru/nasrulaev/spring/controllers/PeopleController.java
+++ src/main/java/ru/nasrulaev/spring/controllers/PeopleController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.nasrulaev.spring.services.PeopleService;
 
@@ -23,5 +24,11 @@ public class PeopleController {
     public String index(Model model) {
         model.addAttribute("people", peopleService.findAll());
         return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", peopleService.findOne(id));
+        return "people/show";
     }
 }
