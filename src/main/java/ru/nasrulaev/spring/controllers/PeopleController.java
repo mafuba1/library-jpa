4ef7fb1c0369ca1463e51d4b9585922e64f295ca
mafuba <tulkidg@gmail.com>
2023-09-09 17:59:02 +0300

    feat(controller): add index method

--- src/main/java/ru/nasrulaev/spring/controllers/PeopleController.java
+++ src/main/java/ru/nasrulaev/spring/controllers/PeopleController.java
@@ -2,6 +2,8 @@ package ru.nasrulaev.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.nasrulaev.spring.services.PeopleService;
 
@@ -16,4 +18,10 @@ public class PeopleController {
         this.peopleService = peopleService;
     }
 
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("people", peopleService.findAll());
+        return "people/index";
+    }
 }
