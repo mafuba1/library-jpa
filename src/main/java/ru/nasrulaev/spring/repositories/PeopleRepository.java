623500d19884fcc6f5e252e56243ffb322f0d15a
mafuba <tulkidg@gmail.com>
2023-09-09 18:47:18 +0300

    feat(repo): add existsPersonByName method

--- src/main/java/ru/nasrulaev/spring/repositories/PeopleRepository.java
+++ src/main/java/ru/nasrulaev/spring/repositories/PeopleRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.nasrulaev.spring.models.Person;
 
+
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    boolean existsPersonByName(String name);
 }
