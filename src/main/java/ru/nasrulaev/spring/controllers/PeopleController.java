340842db38997ec9c3a83f29a6223c1d5cfc5837
mafuba <tulkidg@gmail.com>
2023-09-09 17:58:30 +0300

    feat(controller): inject people service

--- src/main/java/ru/nasrulaev/spring/controllers/PeopleController.java
+++ src/main/java/ru/nasrulaev/spring/controllers/PeopleController.java
@@ -1,9 +1,19 @@
 package ru.nasrulaev.spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.nasrulaev.spring.services.PeopleService;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
 }
