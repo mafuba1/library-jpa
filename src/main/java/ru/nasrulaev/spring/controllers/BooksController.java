d401498798ebbbb09f7f4f7cfd4511b62a4df6cd
mafuba <tulkidg@gmail.com>
2023-09-11 16:43:13 +0300

    feat(controller): add update method

--- src/main/java/ru/nasrulaev/spring/controllers/BooksController.java
+++ src/main/java/ru/nasrulaev/spring/controllers/BooksController.java
@@ -3,9 +3,9 @@ package ru.nasrulaev.spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.nasrulaev.spring.models.Book;
 import ru.nasrulaev.spring.services.BooksService;
 import ru.nasrulaev.spring.validators.BookValidator;
 
@@ -39,5 +39,17 @@ public class BooksController {
     public String edit(@PathVariable("id") int id, Model model) {
         model.addAttribute("book", booksService.findOne(id));
         return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") Book book, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        bookValidator.validate(book, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "redirect:/books";
+
+        booksService.update(id, book);
+        return "redirect:/books";
     }
 }
