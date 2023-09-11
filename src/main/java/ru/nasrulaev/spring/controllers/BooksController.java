3906c033149a7e6f055865729c123a04f30efb16
mafuba <tulkidg@gmail.com>
2023-09-11 16:38:25 +0300

    feat(controller): add show method

--- src/main/java/ru/nasrulaev/spring/controllers/BooksController.java
+++ src/main/java/ru/nasrulaev/spring/controllers/BooksController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.nasrulaev.spring.services.BooksService;
 import ru.nasrulaev.spring.validators.BookValidator;
@@ -26,5 +27,11 @@ public class BooksController {
     public String index(Model model) {
         model.addAttribute("books", booksService.findAll());
         return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", booksService.findOne(id));
+        return "books/show";
     }
 }
