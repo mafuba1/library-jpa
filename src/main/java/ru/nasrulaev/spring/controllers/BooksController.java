5d5c2baceb97b7258d3fcfa19a62a201bdd3e132
mafuba <tulkidg@gmail.com>
2023-09-11 16:34:09 +0300

    fix(controller): inject BooksService
    Remove PeopleService injection

--- src/main/java/ru/nasrulaev/spring/controllers/BooksController.java
+++ src/main/java/ru/nasrulaev/spring/controllers/BooksController.java
@@ -1,9 +1,22 @@
 package ru.nasrulaev.spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.nasrulaev.spring.services.BooksService;
+import ru.nasrulaev.spring.validators.BookValidator;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
+
+    private final BooksService booksService;
+    private final BookValidator bookValidator;
+
+
+    @Autowired
+    public BooksController(BooksService booksService, BookValidator bookValidator) {
+        this.booksService = booksService;
+        this.bookValidator = bookValidator;
+    }
 }
